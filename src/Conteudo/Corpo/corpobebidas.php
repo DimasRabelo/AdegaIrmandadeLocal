0743309de586fb217290f1f44d9f517a17071e38
sowie <dimas.rabelosouza@gmail.com>
2024-03-23 13:57:53 -0300

    update

--- src/Conteudo/Corpo/corpobebidas.php
+++ src/Conteudo/Corpo/corpobebidas.php
@@ -1,9 +1,3 @@
-<style>
-
-</style>
-
-
-
 <div class="nomebebidas">
     <h2>PRODUTOS</h2>
 </div>
@@ -19,47 +13,59 @@
 
             <div class="Cervejas">
                 <div>
-                    <a href="src/imagens/bebidas/cerveja220/skol269.png" data-lightbox="gallery" data-title="Skol 269ML">
+                    <a href="src/imagens/bebidas/cerveja220/skol269.png" data-lightbox="roadtrip" data-title="Skol 269ML">
                         <img src="src/imagens/bebidas/cerveja220/skol269.png" alt="">
                     </a>
                     <p>Skol 269ML</p>
                 </div>
 
                 <div>
-                    <a href="src/imagens/bebidas/cerveja220/Amstel269ml.png" data-lightbox="gallery" data-title="Amstel 269ML">
+                    <a href="src/imagens/bebidas/cerveja220/Amstel269ml.png" data-lightbox="roadtrip" data-title="Amstel 269ML">
                         <img src="src/imagens/bebidas/cerveja220/Amstel269ml.png" alt="">
                     </a>
                     <p>Amstel 269ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/cerveja220/brahmaduplomalte269ml.png" alt="">
-                    <p>Brahma Duplo Malte 269ML</p>
+                    <a href="src/imagens/bebidas/cerveja220/brahmaduplomalte269ml.png" data-lightbox="roadtrip" data-title="BrahmaDuploMalte 269ML">
+                        <img src="src/imagens/bebidas/cerveja220/brahmaduplomalte269ml.png" alt="">
+                    </a>
+                    <p>Brahma Duplo 269ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/cerveja220/coronalongneck.png" alt="">
+                    <a href="src/imagens/bebidas/cerveja220/coronalongneck.png" data-lightbox="roadtrip" data-title="Corona Long Neck 330ML">
+                        <img src="src/imagens/bebidas/cerveja220/coronalongneck.png" alt="">
+                    </a>
                     <p>Corona Long Neck 330ML</p>
                 </div>
 
 
                 <div>
-                    <img src="src/imagens/bebidas/cerveja220/heinekenlongneck.png" alt="">
+                    <a href="src/imagens/bebidas/cerveja220/heinekenlongneck.png" data-lightbox="roadtrip" data-title="Heineken Long Neck 330ML">
+                        <img src="src/imagens/bebidas/cerveja220/heinekenlongneck.png" alt="">
+                    </a>
                     <p>Heineken Long Neck 330ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/cerveja220/itaipava269ml.png" alt="">
+                    <a href="src/imagens/bebidas/cerveja220/itaipava269ml.png" data-lightbox="roadtrip" data-title="Itaipava 269ML">
+                        <img src="src/imagens/bebidas/cerveja220/itaipava269ml.png" alt="">
+                    </a>
                     <p>Itaipava 269ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/cerveja220/original269ml.png" alt="">
+                    <a href="src/imagens/bebidas/cerveja220/original269ml.png" data-lightbox="roadtrip" data-title="Original 269ML">
+                        <img src="src/imagens/bebidas/cerveja220/original269ml.png" alt="">
+                    </a>
                     <p>Original 269ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/cerveja220/spaten269.png" alt="">
+                    <a href="src/imagens/bebidas/cerveja220/spaten269.png" data-lightbox="roadtrip" data-title="Spaten 269ML">
+                        <img src="src/imagens/bebidas/cerveja220/spaten269.png" alt="">
+                    </a>
                     <p>Spaten 269ML</p>
                 </div>
 
@@ -68,7 +74,9 @@
 
                 <?php if (!empty($lista)) : ?>
                     <div class="bannerBreja">
-                        <img src="<?php echo 'src/imagens/' . $lista[0]['fotoBanner']; ?>" alt="<?php echo $lista[0]['altBanner']; ?>">
+                        <a href="src/imagens/<?php echo $lista[0]['fotoBanner']; ?>" class="lightbox" data-lightbox="cervejas" data-title="<?php echo $lista[0]['altBanner']; ?>">
+                            <img src="src/imagens/<?php echo $lista[0]['fotoBanner']; ?>" alt="<?php echo $lista[0]['altBanner']; ?>">
+                        </a>
                     </div>
                 <?php endif; ?>
 
@@ -78,14 +86,6 @@
 </div>
 
 
-
-
-
-
-
-
-
-
 <div class="sample-content" id="product2">
     <div class="site">
         <div class="Refrigerantes">
@@ -93,30 +93,42 @@
 
 
             <section>
+
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/bally.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/bally.png" data-lightbox="roadtrip" data-title="Energetico Bally 2 Litros">
+                        <img src="src/imagens/bebidas/refrigerantes220/bally.png" alt="">
+                    </a>
                     <p>Energético Bally 2 Litros</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/coca-cola-2-litros.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/coca-cola-2-litros.png" data-lightbox="roadtrip" data-title="Coca Cola 2 Litros">
+                        <img src="src/imagens/bebidas/refrigerantes220/coca-cola-2-litros.png" alt="">
+                    </a>
                     <p>Coca Cola 2 Litros</p>
+
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/cocacola350ml.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/cocacola350ml.png" data-lightbox="roadtrip" data-title="Coca Lata 350ML">
+                        <img src="src/imagens/bebidas/refrigerantes220/cocacola350ml.png" alt="">
+                    </a>
                     <p>Coca Lata 350ML</p>
                 </div>
 
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/dolly.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/dolly.png" data-lightbox="roadtrip" data-title=" Dolly 2 Litros">
+                        <img src="src/imagens/bebidas/refrigerantes220/dolly.png" alt="">
+                    </a>
                     <p>Dolly 2 Litros</p>
                 </div>
 
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/fanta2litros.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/fanta2litros.png" data-lightbox="roadtrip" data-title="Fanta 2 litros">
+                        <img src="src/imagens/bebidas/refrigerantes220/fanta2litros.png" alt="">
+                    </a>
                     <p>Fanta 2 litros</p>
                 </div>
             </section>
@@ -126,27 +138,37 @@
 
             <section>
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/fanta350ml.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/fanta350ml.png" data-lightbox="roadtrip" data-title="Fanta Lata 350ML">
+                        <img src="src/imagens/bebidas/refrigerantes220/fanta350ml.png" alt="">
+                    </a>
                     <p>Fanta Lata 350ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/gatorade.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/gatorade.png" data-lightbox="roadtrip" data-title="Gatorade 500ML">
+                        <img src="src/imagens/bebidas/refrigerantes220/gatorade.png" alt="">
+                    </a>
                     <p>Gatorade 500ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/gelodecoco.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/gelodecoco.png" data-lightbox="roadtrip" data-title="Gelo de Coco">
+                        <img src="src/imagens/bebidas/refrigerantes220/gelodecoco.png" alt="">
+                    </a>
                     <p>Gelo de Coco</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/gelodesabormelancia.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/gelodesabormelancia.png" data-lightbox="roadtrip" data-title="Gelo Sabor Melancia">
+                        <img src="src/imagens/bebidas/refrigerantes220/gelodesabormelancia.png" alt="">
+                    </a>
                     <p>Gelo Sabor Melancia</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/guaraviton.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/guaraviton.png" data-lightbox="roadtrip" data-title="Guaraviton 500ML">
+                        <img src="src/imagens/bebidas/refrigerantes220/guaraviton.png" alt="">
+                    </a>
                     <p>Guaraviton 500ML</p>
                 </div>
             </section>
@@ -154,27 +176,37 @@
 
             <section>
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/h20.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/h20.png" data-lightbox="roadtrip" data-title="H2O 500ML">
+                        <img src="src/imagens/bebidas/refrigerantes220/h20.png" alt="">
+                    </a>
                     <p>H2O 500ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/images.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/images.png" data-lightbox="roadtrip" data-title="Chop de Vinho 600ML">
+                        <img src="src/imagens/bebidas/refrigerantes220/images.png" alt="">
+                    </a>
                     <p>Chop de Vinho 600ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/redbul.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/redbul.png" data-lightbox="roadtrip" data-title="Red Bull 250ML">
+                        <img src="src/imagens/bebidas/refrigerantes220/redbul.png" alt="">
+                    </a>
                     <p>Red Bull 250ML </p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/vibe.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/vibe.png" data-lightbox="roadtrip" data-title="Energético Vibe 2 Litros">
+                        <img src="src/imagens/bebidas/refrigerantes220/vibe.png" alt="">
+                    </a>
                     <p>Energético Vibe 2 Litros</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/bebidas/refrigerantes220/pepsi.png" alt="">
+                    <a href="src/imagens/bebidas/refrigerantes220/pepsi.png" data-lightbox="roadtrip" data-title="Pepsi 2 Litros">
+                        <img src="src/imagens/bebidas/refrigerantes220/pepsi.png" alt="">
+                    </a>
                     <p>Pepsi 2 Litros</p>
                 </div>
             </section>
@@ -182,16 +214,7 @@
 
     </div>
 
-
 </div>
-
-
-
-
-
-
-
-
 
 <div class="sample-content" id="product3">
     <div class="site">
@@ -200,22 +223,30 @@
         <div class="Destilados">
 
             <div>
-                <img src="src/imagens/bebidas/fortes220/ballantines.png" alt="">
+                <a href="src/imagens/bebidas/fortes220/ballantines.png" data-lightbox="roadtrip" data-title="Ballantines">
+                    <img src="src/imagens/bebidas/fortes220/ballantines.png" alt="">
+                </a>
                 <p>Ballantines</p>
             </div>
 
             <div>
-                <img src="src/imagens/bebidas/fortes220/black label.png" alt="">
+                <a href="src/imagens/bebidas/fortes220/black label.png" data-lightbox="roadtrip" data-title="Black Label">
+                    <img src="src/imagens/bebidas/fortes220/black label.png" alt="">
+                </a>
                 <p>Black Label</p>
             </div>
 
             <div>
-                <img src="src/imagens/bebidas/fortes220/gin rocks.png" alt="">
+                <a href="src/imagens/bebidas/fortes220/gin rocks.png" data-lightbox="roadtrip" data-title="Gin Rocks">
+                    <img src="src/imagens/bebidas/fortes220/gin rocks.png" alt="">
+                </a>
                 <p>Gin Rocks</p>
             </div>
 
             <div>
-                <img src="src/imagens/bebidas/fortes220/gininvictus.png" alt="">
+                <a href="src/imagens/bebidas/fortes220/gininvictus.png" data-lightbox="roadtrip" data-title="Gin Invictus">
+                    <img src="src/imagens/bebidas/fortes220/gininvictus.png" alt="">
+                </a>
                 <p>Gin Invictus</p>
             </div>
 
@@ -226,7 +257,9 @@
     <div class="bannerDestilado">
         <?php if (!empty($lista)) : ?>
             <div>
-                <img src="<?php echo 'src/imagens/' . $lista[1]['fotoBanner']; ?>" alt="<?php echo $lista[1]['altBanner']; ?>">
+                <a href="src/imagens/<?php echo $lista[1]['fotoBanner']; ?>" class="lightbox" data-lightbox="destilados" data-title="<?php echo $lista[1]['altBanner']; ?>">
+                    <img src="src/imagens/<?php echo $lista[1]['fotoBanner']; ?>" alt="<?php echo $lista[1]['altBanner']; ?>">
+                </a>
             </div>
         <?php endif; ?>
     </div>
@@ -234,22 +267,30 @@
         <div class="Destilados2">
 
             <div>
-                <img src="src/imagens/bebidas/fortes220/jackdaniels.png" alt="">
+                <a href="src/imagens/bebidas/fortes220/jackdaniels.png" data-lightbox="roadtrip" data-title="Jack Daniels">
+                    <img src="src/imagens/bebidas/fortes220/jackdaniels.png" alt="">
+                </a>
                 <p>Jack Daniels</p>
             </div>
 
             <div>
-                <img src="src/imagens/bebidas/fortes220/redlabel.png" alt="">
+                <a href="src/imagens/bebidas/fortes220/redlabel.png" data-lightbox="roadtrip" data-title="Red Label">
+                    <img src="src/imagens/bebidas/fortes220/redlabel.png" alt="">
+                </a>
                 <p>Red Label</p>
             </div>
 
             <div>
-                <img src="src/imagens/bebidas/fortes220/whitehorse.png" alt="">
+                <a href="src/imagens/bebidas/fortes220/whitehorse.png" data-lightbox="roadtrip" data-title="White Horse">
+                    <img src="src/imagens/bebidas/fortes220/whitehorse.png" alt="">
+                </a>
                 <p>White Horse</p>
             </div>
 
             <div>
-                <img src="src/imagens/bebidas/fortes220/taquery.png" alt="">
+                <a href="src/imagens/bebidas/fortes220/taquery.png" data-lightbox="roadtrip" data-title="Taquery">
+                    <img src="src/imagens/bebidas/fortes220/taquery.png" alt="">
+                </a>
                 <p>Taquery</p>
 
             </div>
@@ -258,12 +299,4 @@
 
     </div>
 
-
-
-
-
-
-
-
-</div>
 </div>
