06f30482941b5e4255579f503e95e98e3bf519d6
sowie <dimas.rabelosouza@gmail.com>
2024-03-31 22:05:30 -0300

    update

--- src/Conteudo/Corpo/corpogaleria.php
+++ src/Conteudo/Corpo/corpogaleria.php
@@ -2,8 +2,8 @@
 <section class="galeria wow animate__animated animate__fadeInUp">
 
     <?php for ($i = 0; $i < count($lista); $i++) : ?>
-    
-            <img src="<?php echo 'src/imagens/' . $lista[$i]['fotoGaleria'] ?>" alt="">
+
+        <img src="<?php echo 'src/imagens/' . $lista[$i]['fotoGaleria'] ?>" alt="">
 
     <?php endfor; ?>
 
@@ -23,9 +23,9 @@
             <div class="carousel-item">
                 <iframe class="video" width="100%" height="600" src="https://www.youtube.com/embed/wFP3Vte7mtY?enablejsapi=1&rel=0" title="Video 2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
-            <div class="carousel-item">
-                <iframe class="video" width="100%" height="600" src="https://www.youtube.com/embed/Kskj9xfOCY?enablejsapi=1&rel=0" title="Video 3" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
+           
+
+
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -42,7 +42,7 @@
     var videos = document.querySelectorAll('.video');
 
     function stopVideos() {
-        videos.forEach(function (video) {
+        videos.forEach(function(video) {
             video.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
         });
     }
@@ -50,4 +50,3 @@
     var carousel = document.getElementById('carouselExample');
     carousel.addEventListener('slid.bs.carousel', stopVideos);
 </script>
-
