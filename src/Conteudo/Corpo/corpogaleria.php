9286aaf7761f5557b08f52ba4ccac69b0c0a18e5
sowie <dimas.rabelosouza@gmail.com>
2024-04-08 06:17:33 -0300

    update

--- src/Conteudo/Corpo/corpogaleria.php
+++ src/Conteudo/Corpo/corpogaleria.php
@@ -24,10 +24,10 @@
                 <iframe class="video" width="100%" height="600" src="https://www.youtube.com/embed/wFP3Vte7mtY?enablejsapi=1&rel=0" title="Video 2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div class="carousel-item">
-                <iframe width="100%" height="600" src="https://www.youtube.com/embed/Vu1HFZ-8vII?enablejsapi=1&rel=0" title="Adega Lounge e Tabacaria Irmandade Localizado na rua Manoel Bueno da fonseca,46" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe class="video" width="100%" height="600" src="https://www.youtube.com/embed/Vu1HFZ-8vII?enablejsapi=1&rel=0" title="Adega Lounge e Tabacaria Irmandade Localizado na rua Manoel Bueno da fonseca,46" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
             <div class="carousel-item">
-            <iframe width="100%" height="600" src="https://www.youtube.com/embed/Kskj9xfO2CY?enablejsapi=1&rel=0" title="22 de setembro de 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe class="video"   width="100%" height="600" src="https://www.youtube.com/embed/Kskj9xfO2CY?enablejsapi=1&rel=0" title="22 de setembro de 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
 
         </div>
