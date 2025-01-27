752d2678573fe6dd6694e9b4f4b66e7075236e06
Dimas <dimas_ap_souza@yahoo.com.br>
2025-01-27 09:43:04 -0300

    Update corpogaleria.php

--- src/Conteudo/Corpo/corpogaleria.php
+++ src/Conteudo/Corpo/corpogaleria.php
@@ -21,15 +21,14 @@
                 <iframe class="video" width="100%" height="600" src="<?php echo $listaUrl[0]['urlVideo'];?>?enablejsapi=1&rel=0" title="Video 1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div class="carousel-item">
-                <iframe class="video" width="100%" height="600" src=<?php echo $listaUrl[1]['urlVideo'];?>?enablejsapi=1&rel=0" title="Video 2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe class="video" width="100%" height="600" src="<?php echo $listaUrl[1]['urlVideo'];?>?enablejsapi=1&rel=0" title="Video 2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div class="carousel-item">
                 <iframe class="video" width="100%" height="600" src="<?php echo $listaUrl[2]['urlVideo'];?>?enablejsapi=1&rel=0" title="Adega Lounge e Tabacaria Irmandade Localizado na rua Manoel Bueno da fonseca,46" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
             <div class="carousel-item">
-            <iframe class="video"   width="100%" height="600" src="<?php echo $listaUrl[3]['urlVideo'];?>?enablejsapi=1&rel=0" title="22 de setembro de 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe class="video" width="100%" height="600" src="<?php echo $listaUrl[3]['urlVideo'];?>?enablejsapi=1&rel=0" title="22 de setembro de 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
-
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
